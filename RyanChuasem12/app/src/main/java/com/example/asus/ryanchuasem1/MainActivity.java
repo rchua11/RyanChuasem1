92341ef0c702fac3f5c3f4a7ac73f0e3a7d7017e
rchua11 <20120480602@ust-ics.mygbiz.com>
2014-09-09 23:04:48 +0800

    public awareness and preparedness APP

---
+++ RyanChuasem12/app/src/main/java/com/example/asus/ryanchuasem1/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.asus.ryanchuasem1;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+
+        public PlaceholderFragment() {
+        }
+            View rootView;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+             rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            Button btn = (Button)rootView.findViewById(R.id.btnNumber1);
+            btn.setOnClickListener(this);
+
+            return rootView;
+
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            Button button = (Button) v;
+            startActivity(new Intent(getActivity().getApplicationContext(), activity2.class));
+        }
+
+
+    }
+}
